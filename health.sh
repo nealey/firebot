bcbb096d7d9feba52527e141b4a728a6d1a7c8ab
Neale Pickett <neale@lanl.gov>
2007-10-04 16:21:34 -0600

    Make health script suck less

--- health.sh
+++ health.sh
@@ -2,6 +2,6 @@
 ## Restart the bot if it's not running
 
 # Gallium assumes everything's in the cwd
-cd /home/neale/src/firebot
+cd $(basename $0)
 
-kill -0 `cat gallium.pid` 2>/dev/null || ./gallium.py
+kill -0 $(cat gallium.pid) 2>/dev/null || ./gallium.py
