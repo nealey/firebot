0ceef62a223192a32f3d621b8117e0cdf0db034f
Neale Pickett <neale@woozle.org>
2014-07-09 13:16:55 -0600

    Clean this crap up

---
+++ example.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import irc
+
+SERVER = ('irc.synirc.net', 6667)
+NAMES = ['idiot15']
+INFO = "Example bot"
+CHANNELS = ["#idiotbot"]
+
+class IdiotBot(irc.Bot):
+    debug = True
+
+	# Every time anybody says anything, respond like an idiot.
+    def cmd_privmsg(self, sender, forum, addl):
+        forum.msg("I am an idiot!")
+
+l2 = IdiotBot(SERVER, NAMES, INFO, CHANNELS)
+
+irc.run_forever()
